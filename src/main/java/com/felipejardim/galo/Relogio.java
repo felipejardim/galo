3484ca83beb435047ec262f9bf3c90d93fe89ba3
Felipe Lima Jardim <felipe@MacBook-Pro-de-Felipe.local>
2022-07-26 08:48:03 -0400

    adaptando código para o conteúdo passado na aula

--- src/main/java/com/felipejardim/galo/Relogio.java
+++ src/main/java/com/felipejardim/galo/Relogio.java
@@ -1,37 +1,38 @@
 package com.felipejardim.galo;
 
-import java.sql.Date;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class Relogio implements Runnable{
-    private ScheduledExecutorService service = Executors.newScheduledThreadPool(1);
-
-    public Relogio(){}
-    public Relogio(ScheduledExecutorService service){
-        this.service = service;
-    }
-
     @Override
     public void run(){
-        String hora = LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm"));
 
-        switch (hora){
-            case "06:00":
-                System.out.println("BOM DIA");
-                break;
-            case "12:00":
-                System.out.println("BOA TARDE");
-                break;
-            case "18:01":
-                System.out.println("BOA NOITE");
-                break;
+        while(true) {
+            Date data = new Date();
+
+            switch (data.getHours()) {
+                case 8:
+                    System.out.println("BOM DIA");
+                    break;
+                case 12:
+                    System.out.println("BOA TARDE");
+                    break;
+                case 17:
+                    if (data.getMinutes() == 30)
+                        System.out.println("BOA NOITE");
+                    break;
+            }
+
+            try {
+                Thread.sleep(60000);            // aguarda 1min
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
-
-        service.schedule(new Relogio(service),1, TimeUnit.MINUTES);
     }
 }
