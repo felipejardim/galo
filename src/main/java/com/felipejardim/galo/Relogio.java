0ac62e13943123f657e5b9d0d9c087c9ce0625f3
Felipe Lima Jardim <felipe@MacBook-Pro-de-Felipe.local>
2022-07-26 08:57:07 -0400

    refatorado para remover funções depreciadas

--- src/main/java/com/felipejardim/galo/Relogio.java
+++ src/main/java/com/felipejardim/galo/Relogio.java
@@ -1,21 +1,15 @@
 package com.felipejardim.galo;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.Calendar;
 
 public class Relogio implements Runnable{
     @Override
     public void run(){
 
         while(true) {
-            Date data = new Date();
+            Calendar calendario = Calendar.getInstance();
 
-            switch (data.getHours()) {
+            switch (calendario.get(Calendar.HOUR_OF_DAY)) {
                 case 8:
                     System.out.println("BOM DIA");
                     break;
@@ -23,7 +17,7 @@ public class Relogio implements Runnable{
                     System.out.println("BOA TARDE");
                     break;
                 case 17:
-                    if (data.getMinutes() == 30)
+                    if (calendario.get(Calendar.MINUTE) == 30)
                         System.out.println("BOA NOITE");
                     break;
             }
